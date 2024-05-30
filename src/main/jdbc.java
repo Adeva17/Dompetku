41e079e11b6970855f017e9023668bcd1af9ae5e
fithraa <153191400+fithraa@users.noreply.github.com>
2024-05-31 00:29:21 +0700

    Add files via upload

---
+++ src/main/jdbc.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+
+/**
+ *
+ * @author turik
+ */
+public class jdbc {
+  
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/financialmanagerapp";
+    private static final String USER = "root1";
+    private static final String PASS = "root1234";
+    
+    public static Connection connect;
+    //private static Statement statement;
+    //private static ResultSet resultData;
+    
+    public static void connection(){
+        
+        try{
+            Class.forName(JDBC_DRIVER);
+            connect = DriverManager.getConnection(DB_URL, USER, PASS);
+            System.out.println("koneksi berhasil");
+            
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
