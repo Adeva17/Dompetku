ecde94044be43c9a12b431e2b3afc4654f1474ea
fithraa <153191400+fithraa@users.noreply.github.com>
2024-05-31 00:31:31 +0700

    Update jdbc.java

--- src/main/jdbc.java
+++ src/main/jdbc.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+//Libraries = mysql-connector-j-8.4.0.jar
 package main;
 
 import java.sql.Connection;
