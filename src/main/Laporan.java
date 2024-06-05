d8577cf6782ec0d9a9cb96774b539942c42c97ab
Muhammad Adeva <mhmmdadeva@gmail.com>
2024-06-05 23:23:23 +0700

    tombol ok works

--- src/main/Laporan.java
+++ src/main/Laporan.java
@@ -157,6 +157,9 @@ public class Laporan extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        Dashboard select = new Dashboard();
+        this.setVisible(false);
+        select.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
