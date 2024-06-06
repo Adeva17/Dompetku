381ae2ef4808aca3148fd125f2b2d5cd4eb489f9
fithraa <153191400+fithraa@users.noreply.github.com>
2024-06-06 11:10:59 +0700

    Add files via upload

---
+++ src/database/dbkoneksi.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author turik
+ */
+public class dbkoneksi {
+    Connection connect;
+    public dbkoneksi(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://192.168.0.9:3306/dompetku","root","");
+        }
+        catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
+public Connection getConnection(){
+        return connect;
+    }
+}
